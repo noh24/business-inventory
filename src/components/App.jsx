022be858a82f436172e0d429e46696afec092104
Brian <noh24@ymail.com>
2023-02-10 13:56:57 -0500

    add tailwind styling

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,12 +9,12 @@ import MainContent from './MainContent';
 export default function App() {
   return (
     <div className="container mx-auto">
-      <Header>
+      <Header className="flex justify-between items-center px-5">
         <Logo src={logo} alt="logo" width="70px" height="10px" ></Logo>
         <h1 className="text-3xl text-gray-500">Get Your Apple Products</h1>
         <NavBar>
-          <Button>Home</Button>
-          <Button>Inventory</Button>
+          <Button className="border px-3 py-2 rounded bg-blue-500 text-gray-100 hover:bg-blue-600">Home</Button>
+          <Button className="border px-3 py-2 rounded bg-blue-500 text-gray-100 hover:bg-blue-600">Inventory</Button>
         </NavBar>
       </Header>
       <MainContent></MainContent>
