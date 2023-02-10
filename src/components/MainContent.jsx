99386d8d27a3d0bec255b578c9a202d5b67a172d
Brian <noh24@ymail.com>
2023-02-10 15:56:07 -0500

    add gridtable comp

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,36 +1,37 @@
 import React from "react";
 import Header from "./Header";
 import Button from "./Button";
+import GridTable from "./GridTable";
 
 export default function MainContent() {
   return (
     <div>
       <Header className="flex items-end px-5 space-x-4">
         <Button className="border px-[0.3rem] py-[0.6rem] rounded bg-blue-500 text-gray-100 hover:bg-blue-600 space-y-[0.2rem] flex flex-col items-center justify-center">
-          <div class="w-6 h-0.5 bg-gray-100"></div>
-          <div class="w-6 h-0.5 bg-gray-100"></div>
-          <div class="w-6 h-0.5 bg-gray-100"></div>
+          <div className="w-6 h-0.5 bg-gray-100"></div>
+          <div className="w-6 h-0.5 bg-gray-100"></div>
+          <div className="w-6 h-0.5 bg-gray-100"></div>
         </Button>
         <Button className="border px-2 py-2 rounded bg-blue-500 text-gray-100 hover:bg-blue-600 space-y-[0.2rem] ">
           <div className="flex space-x-1 justify-center items-center ">
-            <div class="w-[0.2rem] h-[0.2rem] bg-gray-100"></div>
-            <div class="w-[0.2rem] h-[0.2rem] bg-gray-100"></div>
-            <div class="w-[0.2rem] h-[0.2rem] bg-gray-100"></div>
+            <div className="w-[0.2rem] h-[0.2rem] bg-gray-100"></div>
+            <div className="w-[0.2rem] h-[0.2rem] bg-gray-100"></div>
+            <div className="w-[0.2rem] h-[0.2rem] bg-gray-100"></div>
           </div>
           <div className="flex space-x-1 justify-center items-center">
-            <div class="w-[0.2rem] h-[0.2rem] bg-gray-100"></div>
-            <div class="w-[0.2rem] h-[0.2rem] bg-gray-100"></div>
-            <div class="w-[0.2rem] h-[0.2rem] bg-gray-100"></div>
+            <div className="w-[0.2rem] h-[0.2rem] bg-gray-100"></div>
+            <div className="w-[0.2rem] h-[0.2rem] bg-gray-100"></div>
+            <div className="w-[0.2rem] h-[0.2rem] bg-gray-100"></div>
           </div>
           <div className="flex space-x-1 justify-center items-center">
-            <div class="w-[0.2rem] h-[0.2rem] bg-gray-100"></div>
-            <div class="w-[0.2rem] h-[0.2rem] bg-gray-100"></div>
-            <div class="w-[0.2rem] h-[0.2rem] bg-gray-100"></div>
+            <div className="w-[0.2rem] h-[0.2rem] bg-gray-100"></div>
+            <div className="w-[0.2rem] h-[0.2rem] bg-gray-100"></div>
+            <div className="w-[0.2rem] h-[0.2rem] bg-gray-100"></div>
           </div>
         </Button>
       </Header>
 
-
+      <GridTable></GridTable>
     </div>
   );
 }
