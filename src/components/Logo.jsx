d9fa76e425a1ccf022d269052c1b1adcee24923a
Brian <noh24@ymail.com>
2023-02-10 13:39:50 -0500

    add logo comp

---
+++ src/components/Logo.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Logo({ src, alt, height, width }) {
+  return (
+    <div>
+      <img src={src} alt={alt} height={height} width={width} />
+    </div>
+  )
+}
