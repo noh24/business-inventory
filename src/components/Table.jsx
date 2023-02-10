44a58401f9d64068ec7fdc8c5e39d9c41b0357a7
Brian <noh24@ymail.com>
2023-02-10 18:37:54 -0500

    style table

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -8,9 +8,9 @@ export default function Table() {
   productArray = data.map((entry) => (
     <Product key={entry.id}>
       <tr className="odd:bg-blue-300 odd:text-gray-50">
-        <td className="px-3 py-2">{entry.name}</td>
-        <td className="px-3 py-2">{"$" + entry.price}</td>
-        <td className="px-3 py-2">{entry.quantity}</td>
+        <td className="px-1 py-2">{entry.name}</td>
+        <td className="px-1 py-2">{"$" + entry.price}</td>
+        <td className="px-1 py-2">{entry.quantity}</td>
       </tr>
     </Product>
   ));
@@ -19,7 +19,7 @@ export default function Table() {
     <table className="table-fixed w-full text-center text-md shadow-md shadow-blue-200 mt-14 mb-10">
       <thead className="text-sm uppercase bg-gray-50 rounded-full">
         <tr>
-          <th className="px-3 py-2">Name</th>
+          <th className="px-1 py-2">Name</th>
           <th>Price</th>
           <th>Quantity</th>
         </tr>
