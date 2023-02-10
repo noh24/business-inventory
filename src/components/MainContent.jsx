36f0bd0c5f46016bf8676090a1b3bf9e69aaf4ae
Brian <noh24@ymail.com>
2023-02-10 18:38:09 -0500

    add state & method to toggle display

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,74 +1,29 @@
 import React from "react";
 import Header from "./utility-components/Header";
-import Button from "./utility-components/Button";
 import Carousel from "./Carousel";
 import Table from "./Table";
+import MainContentControl from "./MainContentControl";
 
 export default class MainContent extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      displayList: false,
+      displayList: null,
     }
+  }
+
+  handleToggleDisplay = () => {
+    this.displayList ? this.setState(state => ({ displayList: !state.displayList, })) : this.setState(state => ({ displayList: !state.displayList, }))
   }
 
   render() {
     return (
       <div>
-        <Header className="flex justify-center items-center px-5 space-x-2 mt-10">
-          <Button className="border rounded px-2 py-1 bg-blue-500 text-gray-100 hover:bg-blue-600">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-6 h-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-              />
-            </svg>
-          </Button>
-          <Button className="border rounded px-2 py-1 bg-blue-500 text-gray-100 hover:bg-blue-600">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-6 h-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M10.5 1.5H8.25A2.25 2.25 0 006 3.75v16.5a2.25 2.25 0 002.25 2.25h7.5A2.25 2.25 0 0018 20.25V3.75a2.25 2.25 0 00-2.25-2.25H13.5m-3 0V3h3V1.5m-3 0h3m-3 18.75h3"
-              />
-            </svg>
-          </Button>
-          <Button className="border px-2 py-1 rounded bg-blue-500 text-gray-100 hover:bg-blue-600">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-6 h-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M12 4.5v15m7.5-7.5h-15"
-              />
-            </svg>
-          </Button>
+        <Header className="mt-10">
+          <MainContentControl displayList={this.state.displayList} onToggleDisplay={this.handleToggleDisplay}></MainContentControl>
         </Header>
-
-        <Carousel></Carousel>
-        <Table></Table>
+        {this.state.displayList ? <Table></Table> : <Carousel></Carousel>}
       </div>
     );
   }
